157e093ba147222d9e02ba79b97b87cfa147e6dd
GayathriManeksha <gaya3maneksha@github.com>
2023-01-07 22:21:53 +0530

    Backend done

--- app/database.py
+++ app/database.py
@@ -1,9 +1,38 @@
-from app import db 
+from app import db
+
 
 class userLogin(db.Model):
-   username=db.Column(db.String(20),primary_key=True)
-   passwrd=db.Column(db.String(20))
+    username = db.Column(db.String(20), primary_key=True)
+    passwrd = db.Column(db.String(20))
 
-   def __init__(self,username,passwrd):
-      self.username=username
-      self.passwrd=passwrd
+    def __init__(self, username, passwrd):
+        self.username = username
+        self.passwrd = passwrd
+
+
+class userDetails(db.Model):
+    # id=db.Column('id',db.Integer,primary_key=True)
+    username = db.Column(db.String(20), primary_key=True)
+    name = db.Column(db.String(20))
+    address = db.Column(db.String(20))
+    ph_no = db.Column(db.String(12))
+    part = db.Column(db.String(20))
+
+    def __init__(self, username, name, address, ph_no, part):
+        self.username = username
+        self.name = name
+        self.address = address
+        self.ph_no = ph_no
+        self.part = part
+
+
+class tasks(db.Model):
+    id = db.Column('id', db.Integer, primary_key=True)
+    username = db.Column(db.String(20), db.ForeignKey(userLogin.username))
+    task_desc = db.Column(db.String(20))
+    status = db.Column(db.Integer)
+
+    def __init__(self, username, task_desc):
+        self.username = username
+        self.task_desc = task_desc
+        self.staus = 0
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, flash, url_for, redirect, render_template, json
-from app.database import userLogin
+from app.database import userLogin,userDetails,tasks
 from app import app, db
 
 @app.route('/', methods=['GET', 'POST'])
@@ -11,7 +11,7 @@ def login():
         if user_name == 'Admin':
             return redirect(url_for('view_progress'))
         else:
-            return render_template('')
+            return redirect(url_for('add_details',user_name=user_name))
     return render_template('login.html')
 
 
@@ -33,3 +33,53 @@ def add_mem():
         flash('Record was successfully added')
         return render_template('3rd.html')
     return render_template('3rd.html')
+
+def addtasks(user_name,task_desc):
+    task=tasks(user_name,task_desc)
+    db.session.add(task)
+    db.session.commit()
+    print("Added tasks")
+
+@app.route('/viewdetails/<user_name>',methods=['GET','POST'])
+def view_details(user_name):
+    user = userDetails.query.filter_by(username=user_name).first()
+    task = tasks.query.filter_by(username=user_name).all()
+    if request.method=='POST':
+        print("POST")
+        if request.form.get("add")=='add':
+            addtasks(user_name,request.form['task_desc'])
+        task = tasks.query.filter_by(username=user_name).all()
+        return render_template('page4.html',user=user,task=task)
+    return render_template('page4.html',user=user,task=task)
+
+@app.route('/adddetails/<user_name>',methods=['GET','POST'])
+def add_details(user_name):
+    task = tasks.query.filter_by(username=user_name).all()
+    if request.method=='POST':
+        User=userDetails.query.filter_by(username=user_name).first()
+        User.name=request.form['name']
+        User.address=request.form['address']
+        User.ph_no=request.form['phno']
+        User.part=request.form['part']
+        selitems = request.form.getlist("sel_items")
+        print("selected")
+        print(selitems)
+        for s in selitems:
+            d = tasks.query.filter_by(id=s).first()
+            d.status=1
+        db.session.commit()
+        return render_template('team_mem.html',user=User,task=task)
+    
+    User = userDetails.query.filter_by(username=user_name).first()
+    if User is not None:
+        return render_template('team_mem.html', user=User,task=task)
+        
+    User = userDetails(
+        user_name, " "," "," "," ")
+    try:
+            db.session.add(User)
+            db.session.commit()
+            print('Added successfully')
+            return render_template('team_mem.html', user=User)
+    except:
+            return "Error adding to database"
